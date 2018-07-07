244c523c668371092a0e4932e34839d53b5711d9
Vitalina <vita.sakhniuk@gmail.com>
2018-07-07 16:24:39 +0300

    This is my second commit

---
+++ src/components/Main.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
+const Repair = styled.div `
+color: #ffffff;
+font-size: 40px;
+line-height: 50px;
+font-weight: 700;
+
+span{
+	font-size: 24px;
+	display: block;
+    line-height: 30px;
+}
+
+`
+const LightText= styled.div`
+color: #ffffff;
+margin-top:54px;
+font-size: 18px;
+font-weight: 300;
+line-height: 28px;
+`
+
+const Connect =styled.button`
+width: 247px;
+height: 67px;
+border-radius:30px;
+background-color: #ffa14b;
+margin-top:32px;
+color: #ffffff;
+font-size: 18px;
+font-weight: bold;
+line-height: 24px;
+}`
+class Main extends React.Component{
+	render(){
+		return(
+<Row>
+	<Col lg={5}>
+	<Repair> 
+		Качественный ремонт
+<span>iphone за 35 минут и гарантия 1 год</span>
+	</Repair>
+	<LightText>
+		Оставьте заявку на бесплатную диагностику без очереди,
+и получите защитное стекло, стоимостью 1000 рублей,
+с установкой в подарок!
+	</LightText>
+	<Connect>
+	Оставить заявку!
+	</Connect>
+	</Col>
+
+	<Col lg={6} lgOffset={1}>
+	</Col>
+
+</Row>
+			)
+	}
+}
+export default Main
